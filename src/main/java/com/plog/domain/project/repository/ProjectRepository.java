001b41698b068b96a9be12657d46e3367f7dd485
SangwanYu <ryusangwan12@gmail.com>
2026-07-20 15:21:56 +0900

    [FEAT] #31: 초대 토큰 후보 충돌 잠금 추가

--- src/main/java/com/plog/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/plog/domain/project/repository/ProjectRepository.java
@@ -3,9 +3,16 @@ package com.plog.domain.project.repository;
 import com.plog.domain.project.entity.Project;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findByInviteTokenHash(String inviteTokenHash);
+
+    boolean existsByInviteTokenHash(String inviteTokenHash);
+
+    @Query(value = "select pg_advisory_xact_lock(:lockKey)", nativeQuery = true)
+    void acquireInviteTokenCandidateLock(@Param("lockKey") long lockKey);
 }
