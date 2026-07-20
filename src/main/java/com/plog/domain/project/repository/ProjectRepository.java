aa588ac3c07ce34a5a3bc30186748eb6cc4bef36
SangwanYu <ryusangwan12@gmail.com>
2026-07-20 15:21:31 +0900

    [FEAT] #31: 초대 토큰 해시 조회 저장소 추가

--- src/main/java/com/plog/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/plog/domain/project/repository/ProjectRepository.java
@@ -1,9 +1,11 @@
 package com.plog.domain.project.repository;
 
 import com.plog.domain.project.entity.Project;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Optional<Project> findByInviteTokenHash(String inviteTokenHash);
 }
