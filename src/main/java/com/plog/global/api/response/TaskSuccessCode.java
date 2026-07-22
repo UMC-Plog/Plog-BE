0c9b4ad1925ff77b9ac5c5509f58882dec0bb836
songmin0111 <violet2111@naver.com>
2026-07-23 05:45:49 +0900

    [FEAT] #112: 업무카드 삭제 API 구현

--- src/main/java/com/plog/global/api/response/TaskSuccessCode.java
+++ src/main/java/com/plog/global/api/response/TaskSuccessCode.java
@@ -12,7 +12,8 @@ public enum TaskSuccessCode implements BaseCode {
     TASK_CREATED(HttpStatus.CREATED, "TASK_SUCCESS_001", "업무카드가 생성되었습니다."),
     TASK_LIST_FOUND(HttpStatus.OK, "TASK_SUCCESS_002", "업무카드 목록 조회에 성공했습니다."),
     TASK_DETAIL_FOUND(HttpStatus.OK, "TASK_SUCCESS_003", "업무카드 상세 조회에 성공했습니다."),
-    TASK_UPDATED(HttpStatus.OK, "TASK_SUCCESS_004", "업무카드가 수정되었습니다.");
+    TASK_UPDATED(HttpStatus.OK, "TASK_SUCCESS_004", "업무카드가 수정되었습니다."),
+    TASK_DELETED(HttpStatus.OK, "TASK_SUCCESS_005", "업무카드가 삭제되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
